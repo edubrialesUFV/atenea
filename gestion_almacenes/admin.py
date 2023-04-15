a75e2b2aa37a2a2d93d74b80411ec23835042dd0
Eduardo Briales García <7836054@alumnos.ufv.es>
2023-04-15 20:42:37 +0200

    solucionando

--- gestion_almacenes/admin.py
+++ gestion_almacenes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from gestion_almacenes.models import Producto, Cliente, Pedido, Proveedor, ProductoPosicion, Posicion
 # Register your models here.
+
+
+
+# class AuthorAdmin(admin.ModelAdmin):
+#     pass
+
+admin.site.register(Proveedor)
+admin.site.register(Producto)
+admin.site.register(Cliente)
+admin.site.register(Pedido)
+admin.site.register(Posicion)
