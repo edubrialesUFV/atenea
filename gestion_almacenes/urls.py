7d8c74e563f8b99cd3d50b06fe11dec1cba4a49f
alvaromartos1 <alvaromartos2011@hotmail.com>
2023-04-19 01:21:24 +0200

    generados ficheros para el main view, falta implementar

--- gestion_almacenes/urls.py
+++ gestion_almacenes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
    path('<str:id>', views.producto_detail, name="detail"),
+   path('mostrar_productos/', views.mostrar_productos, name='mostrar_productos'),
 ]
--- gestion_almacenes/views.py
+++ gestion_almacenes/views.py
@@ -16,4 +16,21 @@ def producto_detail(request, id):
     producto = get_object_or_404(models.Producto, referencia=id)
     return render(request, "product_detail.html", {'producto': producto}) 
 
-    
+from django.shortcuts import render
+from .models import Producto
+import pandas as pd
+
+def mostrar_productos(request):
+    # Ruta relativa al archivo Excel desde el directorio raíz del proyecto
+    ruta_excel = 'Proov_Ref_2023-1.xlsx'
+
+    # Leer el archivo Excel utilizando pandas
+    dataframe = pd.read_excel(ruta_excel)
+
+    # Obtener los datos necesarios del dataframe
+    productos = dataframe['Producto'].tolist()
+    caracteristicas = dataframe['Caracteristicas'].tolist()
+
+    # Renderizar una respuesta en el navegador con los datos obtenidos
+    return render(request, 'mostrar_productos.html', {'productos': productos, 'caracteristicas': caracteristicas})
+
