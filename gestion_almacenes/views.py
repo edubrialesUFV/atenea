14f847a77b61089aa1e62117ebafce137e7d2b38
Alonso Sánchez-Muliterno <alonso.sm00@gmail.com>
2023-04-19 12:07:47 +0200

    updating main view

--- gestion_almacenes/views.py
+++ gestion_almacenes/views.py
@@ -5,9 +5,43 @@ from django.shortcuts import render
 from .models import Producto
 # Create your views here.
 
+from .forms import FiltroProveedorForm
+from django.db.models import Q
+
+from .forms import FiltroProveedorForm
+from django.db.models import Q
+
 def index(request):
+    proveedores = Producto.objects.values_list('proveedor', flat=True).distinct()
+
+    form = FiltroProveedorForm(list(proveedores))
     productos = Producto.objects.all()
-    return render(request, "index.html", {'productos': productos})
+
+    if request.method == 'POST':
+        form = FiltroProveedorForm(list(proveedores), request.POST)
+        if form.is_valid():
+            proveedor_seleccionado = form.cleaned_data['proveedor']
+            if proveedor_seleccionado:
+                productos = productos.filter(proveedor=proveedor_seleccionado)
+
+    productos_modificados = []
+
+    for producto in productos:
+        referencia_modificada = producto.referencia.split('_')[0].capitalize()
+        producto_dict = {
+            'referencia': referencia_modificada,
+            'proveedor': producto.proveedor,
+            'cantidad_stock': producto.cantidad_stock,
+            'peso_por_unidad': producto.peso_por_unidad,
+        }
+        productos_modificados.append(producto_dict)
+
+    return render(request, 'index.html', {'productos': productos_modificados, 'form': form})
+
+
+
+
+
 
 
 @staff_member_required
